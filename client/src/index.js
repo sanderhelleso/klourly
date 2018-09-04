b58d15ae095c389c9345d2681d09604d54ed1a4c
Sanderhelleso <sanderhelleso@hotmail.com>
2018-09-03 18:51:50 -0700

    added base components folder

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/main.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./components/App";
+
+ReactDOM.render(<App />,document.getElementById('root'));
 registerServiceWorker();
