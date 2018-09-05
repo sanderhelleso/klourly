dab2f3bba0c1d60919e35edb20b822abd3a22402
Sanderhelleso <sanderhelleso@hotmail.com>
2018-09-05 09:26:47 -0700

    added signup cover img and grid

--- client/src/components/signup/Signup.js
+++ client/src/components/signup/Signup.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
+import './styles/signup.css';
+
 export default class Signup extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello</h1>
-            </div>
+            <main>
+                <div className="row">
+                    <div id="signup-img-cont" className="col l5 m5 s12">
+                    </div>
+                    <div id="singup-form-cont" className="col l7 m7 s12">
+                        <div className="container">
+                            <h2>Hello2</h2>
+                        </div>
+                    </div>
+                </div>
+            </main>
         )
     }
 }
