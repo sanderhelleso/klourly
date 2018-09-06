71aaf458c32f5282cb48e39132a556a12ed719cf
Sanderhelleso <sanderhelleso@hotmail.com>
2018-09-05 18:40:39 -0700

    remove img

--- client/src/components/signup/Signup.js
+++ client/src/components/signup/Signup.js
@@ -6,10 +6,11 @@ export default class Signup extends Component {
     render() {
         return (
             <main>
+                <h2 id="signup-logo">Klourly</h2>
                 <div className="row">
-                    <div id="signup-img-cont" className="col l5 m5 s12">
+                    <div id="signup-img-cont" className="col l6 m5 s12">
                     </div>
-                    <div id="singup-form-cont" className="col l7 m7 s12">
+                    <div id="singup-form-cont" className="col l6 m7 s12">
                         <div className="container">
                             <h2>Hello2</h2>
                         </div>
