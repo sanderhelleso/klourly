fdf1664ec6f1e651a9660970137028742ecef7ab
Sanderhelleso <sanderhelleso@hotmail.com>
2018-09-05 20:41:53 -0700

    now renders heading if scrolling

--- client/src/components/signup/Signup.js
+++ client/src/components/signup/Signup.js
@@ -3,10 +3,60 @@ import React, { Component } from 'react';
 import './styles/signup.css';
 
 export default class Signup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          display_headng: true
+        };
+
+        // bind function to class
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    // add scroll event on mount
+    componentDidMount() {
+        document.querySelector('#signup-form-cont').addEventListener('scroll', this.handleScroll);
+    }
+
+    // remove scroll event on umount
+    componentWillUnmount() {
+        document.querySelector('#signup-form-cont').removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll() {
+
+        // current position of page
+        let currentPosition = document.querySelector('#signup-form-cont').scrollTop
+        console.log(this.state, currentPosition);
+
+        if (currentPosition > 80) {
+            this.setState({
+                display_headng: false
+            });
+        }
+
+        else {
+            this.setState({
+                display_headng: true
+            });
+        }
+    }
+
+    renderHeading() {
+        if (this.state.display_headng === true) {
+            return <h1 id="signup-heading" className="animated fadeIn">Sign Up</h1>;
+        }
+
+        else {
+            return null;
+        }
+    }
+
     render() {
         return (
             <main>
                 <h2 id="signup-logo">Klourly</h2>
+                {this.renderHeading()}
                 <div className="row">
                     <div id="signup-img-cont" className="col l6 m5 s12">
                     </div>
