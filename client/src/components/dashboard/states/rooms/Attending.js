7801792eb22e4183188e3813897a5529aca3bcee
Sanderhelleso <sanderhelleso@hotmail.com>
2018-10-19 23:45:04 -0700

    now render room times

--- client/src/components/dashboard/states/rooms/Attending.js
+++ client/src/components/dashboard/states/rooms/Attending.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ExternalLink, ArrowRight } from 'react-feather';
 
 const mockData = 
 [
@@ -45,26 +46,32 @@ const mockData =
 
 export default class Attending extends Component {
 
-    renderAttendingCards() {
+    renderAttendingRooms() {
         return mockData.map(room => {
             const CARD = 
             <div key={room.name} className="col s4 m6">
                 <div className="card small">
-                <div className="card-image">
-                    <img src={room.cover} />
-                    <span className="card-title">{room.name}</span>
-                </div>
-                <div class="card-fab">
-                    <a className="btn-floating halfway-fab waves-effect waves-light red"></a>
-                </div>
-                <div className="card-content">
-                    <p>{room.location}</p>
-                </div>
+                    <div className="card-image">
+                        <img src={room.cover} />
+                        <span className="card-title room-card-name"><span className="room-card-location">{room.location}</span><br />{room.name}</span>
+                    </div>
+                    <div className="card-fab">
+                        <a className="btn-floating halfway-fab waves-effect waves-light red btn-large"><ArrowRight size={24} /></a>
+                    </div>
+                    <div className="card-content">
+                        {this.renderAttendingRoomTimes(room)}
+                    </div>
                 </div>
             </div>
 
             return CARD;
-        })
+        });
+    }
+
+    renderAttendingRoomTimes(room) {
+        return room.times.map(roomTime => {
+            return <p key={`${room.name}-${roomTime.day}`}>{`${roomTime.day} ${roomTime.timeStart} - ${roomTime.timeEnd}`}</p>
+        });
     }
 
     render() {
@@ -74,7 +81,7 @@ export default class Attending extends Component {
                 <hr />
                 <div className="row">
                     <div className="col s12">
-                        {this.renderAttendingCards()}
+                        {this.renderAttendingRooms()}
                     </div>
                 </div>
             </div>
