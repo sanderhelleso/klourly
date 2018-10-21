798fa01f0d185019ad37eba8a569f3c024d8e890
Sanderhelleso <sanderhelleso@hotmail.com>
2018-10-21 12:05:07 -0700

    added title title for new room

--- client/src/components/dashboard/states/rooms/newRoom/Form.js
+++ client/src/components/dashboard/states/rooms/newRoom/Form.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Form extends Component {
     render() {
         return (
-            <div>
+            <div className="col s10 offset-s1 center">
                 <h5>Form Connected</h5>
             </div>
         )
--- client/src/components/dashboard/states/rooms/newRoom/NewRoom.js
+++ client/src/components/dashboard/states/rooms/newRoom/NewRoom.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import Form from './Form';
+import { redirect } from '../../../../middelware/redirect';
 
 export default class NewRoom extends Component {
+
+    componentWillMount() {
+        document.title = 'Creating a new room - Klourly';
+    }
+
     render() {
         return (
             <main className="container">
                 <div>
-                    <a>Back to dashboard</a>
+                    <a onClick={redirect.dashboard}>Back to dashboard</a>
                 </div>
-                <div>
-                    <h1>New Room</h1>
-                    <p>Lets create a new room</p>
+                <div className="center">
+                    <h1>Lets create a New Room</h1>
+                    <p>A room allow you to keep full controll on whats happening. Who is present, when do people show up, who is the least active and much more.</p>
                 </div>
-                <Form />
+                <div className="row">
+                    <Form />
+                </div>
             </main>
         )
     }
