29d83fa47b6ef4ad7c1c9230685fe4b7ebc839c7
Sanderhelleso <sanderhelleso@hotmail.com>
2018-10-23 17:52:22 -0700

    solved bug regarding timepicker z-index

--- client/src/components/dashboard/states/rooms/newRoom/Days.js
+++ client/src/components/dashboard/states/rooms/newRoom/Days.js
@@ -5,22 +5,66 @@ export default class Days extends Component {
     // open collapsible on mount
     componentDidMount() {
         setTimeout(() => {
-            Array.from(document.querySelectorAll('.collapsible-header'))[this.props.daysID - 1].click();
+            const day = Array.from(document.querySelectorAll('.collapsible-header'))[this.props.daysID - 1];
+            day.click();
         }, 100);
     }
 
     render() {
         console.log(this.props.daysID);
         return (
-            <li className="animated fadeIn">
-                <div className="collapsible-header"><h5>Room Days <span>#{this.props.daysID}</span></h5></div>
-                <div className="collapsible-body">
-                    <p>
-                        <label>
-                        <input type="checkbox" />
-                        <span>Red</span>
-                        </label>
-                    </p>
+            <li>
+                <div className="collapsible-header">
+                    <h5>Room Days <span>#{this.props.daysID}</span></h5>
+                </div>
+                <div className="collapsible-body row">
+                    <div className="col s6">
+                        <p>
+                            <label>
+                            <input type="checkbox" />
+                            <span>Monday</span>
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                            <input type="checkbox" />
+                            <span>Tueseday</span>
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                            <input type="checkbox" />
+                            <span>Wednesday</span>
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                            <input type="checkbox" />
+                            <span>Thursday</span>
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                            <input type="checkbox" />
+                            <span>Friday</span>
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                            <input type="checkbox" />
+                            <span>Saturday</span>
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                            <input type="checkbox" />
+                            <span>Sunday</span>
+                            </label>
+                        </p>
+                    </div>
+                    <div>
+                        <input type="text" className="timepicker" />
+                    </div>
                 </div>
             </li>
         )
