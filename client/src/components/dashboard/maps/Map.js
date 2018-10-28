a38f7a0135633d8a5f37d6518c7d29714fef2c53
Sanderhelleso <sanderhelleso@hotmail.com>
2018-10-28 03:39:11 -0700

    refactored map renderer and added onclick mark pos event

--- client/src/components/dashboard/maps/Map.js
+++ client/src/components/dashboard/maps/Map.js
@@ -3,27 +3,36 @@ import { compose, withStateHandlers } from "recompose";
 import { InfoWindow, withGoogleMap, withScriptjs, GoogleMap, Marker} from "react-google-maps";
 import MapMarker from './MapMarker';
 
-const Map = compose(withScriptjs(withGoogleMap((props) => {
-    
-    function getMapPosOnClick(e) {
+const Map = compose(
+    withStateHandlers(() => ({
+        isMarkerShown: false,
+        markerPosition: null
+    }), {
+        onMapClick: ({ isMarkerShown }) => (e) => ({
+            markerPosition: e.latLng,
+            isMarkerShown: true
+        })
+    }),
+    withScriptjs,
+    withGoogleMap
+) (props =>
+    <div id="maps-cont">
+        <GoogleMap
+            defaultZoom={14}
+            defaultCenter={ { lat: props.coords.lat, lng: props.coords.lng } }
+            onClick={props.onMapClick}
+        >
+            {props.isMarkerShown && <Marker position={props.markerPosition} />}
+            {props.isMarkerShown ? null : <MapMarker location={  { lat: props.coords.lat, lng: props.coords.lng } } />}
+        </GoogleMap>
+    </div>
+);
+
+export default Map;
+
+/*function getMapPosOnClick(e) {
         const lat = e.latLng.lat();
         const lng = e.latLng.lng();
         console.log(lat);
         console.log(lng);
-    }
-                  
-    return  (
-        <div id="maps-cont">
-            <GoogleMap
-                defaultZoom={14}
-                defaultCenter={ { lat: props.coords.lat, lng: props.coords.lng } }
-                onClick={(e) => getMapPosOnClick(e)}
-            >
-            <MapMarker location={  { lat: props.coords.lat, lng: props.coords.lng } } />
-            </GoogleMap>
-        </div>
-        );
-    }
-)));
-
-export default Map;
+    }*/
