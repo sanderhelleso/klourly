be87c71213b2e9047549f9cc94c7acee35ca73a3
Sanderhelleso <sanderhelleso@hotmail.com>
2018-10-30 18:15:57 -0700

    refactores to 1 singel render, utilizing es6 map

--- client/src/components/dashboard/states/rooms/newRoom/stages/Radius.js
+++ client/src/components/dashboard/states/rooms/newRoom/stages/Radius.js
@@ -7,93 +7,56 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { nextStageAction } from '../../../../../../actions/newRoom/nextStageAction';
 
-const staticTxt = {
-    radius50: '50 Meters',
-    radius100: '100 Meters',
-    radius150: '150 Meters',
-    radius200: '200 Meters'
-}
+const className = 'room-option animated fadeIn room-option-stage4-option1 z-depth-2 hoverable no-select';
+const radiusOptions = [
+    {
+        title: '50 Meters',
+        className: 'room-option-stage4-option1',
+        radius: '50'
+    },
+    {
+        title: '100 Meters',
+        className: 'room-option-stage4-option2',
+        radius: '100'
+    },
+    {
+        title: '150 Meters',
+        className: 'room-option-stage4-option3',
+        radius: '150'
+    },
+    {
+        title: '200 Meters',
+        className: 'room-option-stage4-option4',
+        radius: '200'
+    }
+]
 
 class Radius extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            className50: 'room-option animated fadeIn room-option-stage4-option1 z-depth-2 hoverable no-select',
-            className100: 'room-option animated fadeIn room-option-stage4-option2 z-depth-2 hoverable no-select',
-            className150: 'room-option animated fadeIn room-option-stage4-option3 z-depth-2 hoverable no-select',
-            className200: 'room-option animated fadeIn room-option-stage4-option4 z-depth-2 hoverable no-select',
-        }
-
-        this.render50 = this.render50.bind(this);
-        this.render100 = this.render100.bind(this);
-        this.render150 = this.render150.bind(this);
-        this.render200 = this.render200.bind(this);
+        this.renderRadiusOptions = this.renderRadiusOptions.bind(this);
     }
 
-    render50() {
-        return(
-            <div className="col s3">
-                <div 
-                className={this.state.className50} 
-                onClick={(event) => helpers.selectOption(event, {radius: '50'}, this.props)}
-                >
-                    <Compass size={30} />
-                    <h5>{staticTxt.radius50}</h5>
+    renderRadiusOptions() {
+        return radiusOptions.map((option) => {
+            return (
+                <div key={option.radius} className="col s3">
+                    <div 
+                    className={`${className} ${option.className}`} 
+                    onClick={(event) => helpers.selectOption(event, { radius: option.radius }, this.props)}
+                    >
+                        <Compass size={30} />
+                        <h5>{option.title}</h5>
+                    </div>
                 </div>
-            </div>
-        )
-    }
-
-    render100() {
-        return(
-            <div className="col s3">
-                <div 
-                className={this.state.className100} 
-                onClick={(event) => helpers.selectOption(event, {radius: '100'}, this.props)}
-                >
-                    <Compass size={30} />
-                    <h5>{staticTxt.radius100}</h5>
-                </div>
-            </div>
-        )
-    }
-
-    render150() {
-        return(
-            <div className="col s3">
-                <div 
-                className={this.state.className150} 
-                onClick={(event) => helpers.selectOption(event, {radius: '150'}, this.props)}
-                >
-                    <Compass size={30} />
-                    <h5>{staticTxt.radius150}</h5>
-                </div>
-            </div>
-        )
-    }
-
-    render200() {
-        return(
-            <div className="col s3">
-                <div 
-                className={this.state.className200} 
-                onClick={(event) => helpers.selectOption(event, {radius: '200'}, this.props)}
-                >
-                    <Compass size={30} />
-                    <h5>{staticTxt.radius200}</h5>
-                </div>
-            </div>
-        )
+            );
+        })
     }
 
     render() {
         return (
             <div id="room-option-cont" className="col s12 new-room-stage-4">
-                {this.render50()}
-                {this.render100()}
-                {this.render150()}
-                {this.render200()}
+                {this.renderRadiusOptions()}
             </div>
         )
     }
@@ -103,7 +66,6 @@ const mapStateToProps = (state) => {
     return { state };
 };
 
-// update created room state
 const mapDispatchToProps = (dispatch) => {
     return bindActionCreators({ nextStageAction }, dispatch);
 }
