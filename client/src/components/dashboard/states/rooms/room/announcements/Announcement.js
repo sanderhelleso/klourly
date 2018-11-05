5b9b5688a4fec00e08b7f2954f5ed1064895a86a
Sanderhelleso <sanderhelleso@hotmail.com>
2018-11-05 00:52:48 -0800

    now renders readmore btn if body is to long

--- client/src/components/dashboard/states/rooms/room/announcements/Announcement.js
+++ client/src/components/dashboard/states/rooms/room/announcements/Announcement.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import Reactions from './Reactions';
+import { ArrowRight } from 'react-feather';
+
+const ipsums = []
 
 export default class Announcement extends Component {
     constructor() {
@@ -13,13 +16,31 @@ export default class Announcement extends Component {
         }
     }
 
+
+    renderReadMore() {
+
+        if (this.state.body.length > 355) {
+            return (
+                <div className="col s12 announcement-readmore-cont">
+                    <button 
+                    className="announcement-readmore-btn waves-effect waves-light btn animated fadeIn">
+                    Read more
+                    </button>
+                </div>
+            )
+        }
+
+        return null;
+    }
+
     render() {
         return (
             <div className="col s12 animated fadeIn announcement">
-                <h5>{this.state.title}</h5>
-                <span>{this.state.date}</span>
-                <p>{this.state.body}</p>
+                <h5 className="announcement-title">{this.state.title}</h5>
+                <span className="announcement-date">{this.state.date}</span>
+                <p className="announcement-body">{this.state.body.substring(0, 355)}</p>
                 <Reactions />
+                {this.renderReadMore()}
             </div>
         )
     }
