6d368b6e452e35cce2a5b7de075c1ac049771b24
Sanderhelleso <sanderhelleso@hotmail.com>
2018-11-04 22:22:08 -0800

    added enabled/disabled checkin state

--- client/src/components/dashboard/states/rooms/room/Checkin.js
+++ client/src/components/dashboard/states/rooms/room/Checkin.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
 const staticTxt = {
-    attended: 'Total Attended'
+    attended: 'Total Attendence',
+    headingActive: 'Register your attendence',
+    headingDisabled: 'Room currently not active'
+
 }
 
 export default class Attend extends Component {
@@ -9,35 +12,56 @@ export default class Attend extends Component {
         super(props);
 
         this.state = {
+            active: false,
             btnMessage: 'Check In',
             classNameEnabled: 'waves-effect waves-light btn animated fadeIn',
-            classNameDisabled: 'waves-effect waves-light btn animated fadeIn new-room-name-disabled',
-            heading: 'Register your attendence'
+            classNameDisabled: 'waves-effect waves-light btn animated fadeIn new-room-name-disabled'
         }
     }
 
     renderCheckinBtn() {
-        return (
-            <button 
-            id='checkin-btn' 
-            className={this.state.classNameEnabled} 
-            onClick={this.setNextStage}
-            >
-            {this.state.btnMessage}
-            </button>
-        )
+
+        if (this.state.active) {
+
+            return (
+                <div>
+                    <h5>{staticTxt.headingActive}</h5>
+                    <button 
+                    id='checkin-btn' 
+                    className={this.state.classNameEnabled} 
+                    >
+                    {this.state.btnMessageEnabled}
+                    </button>
+                </div>
+            )
+        }
+
+        else {
+
+            return (
+                <div>
+                    <h5>{staticTxt.headingDisabled}</h5>
+                    <button 
+                    id='checkin-btn' 
+                    className={this.state.classNameDisabled} 
+                    onClick={this.setNextStage}
+                    >
+                    {this.state.btnMessage}
+                    </button>
+                </div>
+            )
+        }
     }
 
     render() {
         return (
             <div id="room-checking" className="col s4">
                 <div className="col s12"> 
-                    <h5>{this.state.heading}</h5>
                     {this.renderCheckinBtn()}
                 </div>
                 <div id="room-attendence-cont" className="col s12">
                     <h3 id="room-attendence-percentage">76%</h3>
-                    <p>{staticTxt.attended}</p>
+                    <h5>{staticTxt.attended}</h5>
                 </div>
             </div>
         )
