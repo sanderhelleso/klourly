125a96519e3ed13f5d111956b16cef55b34a07a9
Sanderhelleso <sanderhelleso@hotmail.com>
2018-11-05 22:51:12 -0800

    loader now unmounts when done loading after fade out

--- client/src/components/loaders/LinearLoader.js
+++ client/src/components/loaders/LinearLoader.js
@@ -5,15 +5,41 @@ export default class componentName extends Component {
         super(props);
 
         console.log(props);
+        this.state = {
+            active: true
+        }
+    }
+
+    // unmounts loader when loading state is finished
+    // 800ms timeout to preserve fade out animation
+    componentWillReceiveProps(nextProps) {
+        if (this.props.loading !== nextProps.loading) {
+            setTimeout(() => {
+                this.setState({
+                    active: false
+                });
+            }, 800);
+        }
+    }
+
+    renderLineadLoader() {
+        if (this.state.active) {
+            return (
+                <div className={this.props.loading
+                    ? 'linear-loader-cont animated fadeIn'
+                    : 'linear-loader-cont animated fadeOut'
+                }>
+                    <div className="progress">
+                        <div className="indeterminate"></div>
+                    </div>
+                </div>
+            )
+        }
+
+        return null;
     }
 
     render() {
-        return (
-            <div className={this.props.loading ? 'linear-loader-cont animated fadeIn' : 'linear-loader-cont animated fadeOut'}>
-                <div className="progress">
-                    <div className="indeterminate"></div>
-                </div>
-            </div>
-        )
+        return this.renderLineadLoader();
     }
 }
