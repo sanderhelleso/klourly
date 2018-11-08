79ca830971aba4c67b973fe478978acb7c8ada11
Sanderhelleso <sanderhelleso@hotmail.com>
2018-11-07 21:36:54 -0800

    updated room settings

--- client/src/components/dashboard/states/rooms/room/settings/RoomSettings.js
+++ client/src/components/dashboard/states/rooms/room/settings/RoomSettings.js
@@ -5,8 +5,12 @@ export default class RoomSettings extends Component {
 
     render() {
         return (
-            <div>
-                <h1>This is room settings site</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col s12">
+                        <h1>Settings</h1>
+                    </div>
+                </div>
             </div>
         )
     }
