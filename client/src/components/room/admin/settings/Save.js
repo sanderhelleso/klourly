890aeb6c19aff2e7f2032b97a4ff022fa7b9723c
Sanderhelleso <sanderhelleso@hotmail.com>
2018-11-10 17:32:04 -0800

    added base scollbar color

--- client/src/components/room/admin/settings/Save.js
+++ client/src/components/room/admin/settings/Save.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Save extends Component {
     render() {
         return (
-            <div id="save-room-admin-settings">
+            <div id="save-room-admin-settings" className="z-depth-5">
                 <p>cancel</p>
                 <p>save</p>
             </div>
