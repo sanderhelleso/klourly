58219337cf2d6ee883af0b202d9a5d2f629ce18f
Sanderhelleso <sanderhelleso@hotmail.com>
2018-12-25 17:09:20 -0800

    added settings for announcement form

--- client/src/components/room/admin/announcements/NewAnnouncementModal.js
+++ client/src/components/room/admin/announcements/NewAnnouncementModal.js
@@ -3,10 +3,26 @@ import styled from 'styled-components';
 import { materializeJS } from '../../../../helpers/materialize';
 
 export default class NewAnnouncementModal extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            title: '',
+            message: '',
+            maxLengthTitle: 50,
+            maxLengthMessage: 2000
+        }
+    }
 
     componentDidMount() {
         const modal = document.querySelectorAll('.modal');
         materializeJS.M.Modal.init(modal, {});
+    }
+
+    updateAnnouncement(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
     }
 
     render() {
@@ -21,12 +37,30 @@ export default class NewAnnouncementModal extends Component {
                         <form className="col s12 m8 offset-m2">
                             <div className="row">
                                 <div className="input-field col s12">
-                                    <input id="new-announcement-title" type="text" min-length={1} maxLength={50}/>
+                                    <input 
+                                        required
+                                        name="title"
+                                        id="new-announcement-title" 
+                                        type="text" 
+                                        min-length={1} 
+                                        maxLength={this.state.maxLengthTitle}
+                                        onChange={(e) => this.updateAnnouncement(e)}
+                                    />
                                     <label for="new-announcement-title">Title</label>
+                                    <StyledMessage>{this.state.title.length} / {this.state.maxLengthTitle}</StyledMessage>
                                 </div>
                                 <div class="input-field col s12">
-                                    <textarea id="new-announcement-body" class="materialize-textarea" min-length={1} maxLength={500}></textarea>
+                                    <textarea 
+                                        required
+                                        name="message"
+                                        id="new-announcement-body" 
+                                        className="materialize-textarea" 
+                                        minLength={1} 
+                                        maxLength={this.state.maxLengthMessage}
+                                        onChange={(e) => this.updateAnnouncement(e)}
+                                    ></textarea>
                                     <label for="new-announcement-body">Message</label>
+                                    <StyledMessage>{this.state.message.length} / {this.state.maxLengthMessage}</StyledMessage>
                                 </div>
                             </div>
                         </form>
@@ -56,5 +90,12 @@ const StyledHeader = styled.div`
         font-weight: 400;
         margin-bottom: 2rem;
     }
+`;
+
+const StyledMessage = styled.span`
+    color: #9e9e9e;
+    font-weight: 100;
+    font-size: 0.9rem;
+    float: right;
 `;
 
