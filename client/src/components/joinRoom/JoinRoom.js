206ad724be9dfea3c267b90d70080e13ec78c1ce
Sanderhelleso <sanderhelleso@hotmail.com>
2018-12-27 23:45:17 -0800

    added room owner image to cont

--- client/src/components/joinRoom/JoinRoom.js
+++ client/src/components/joinRoom/JoinRoom.js
@@ -36,15 +36,27 @@ class JoinRoom extends Component {
     renderInvite() {
         if (!this.state.loading) {
             return (
-                <div className="animated fadeIn">
-                    <h1>Join Room</h1>
+                <InvitationCont className="animated fadeIn">
+                    {this.renderInviteImage()}
+                    <h1>{this.state.invitationData.name}</h1>
                     <h5>{this.state.message}</h5>
                     {this.renderInviteActionButton()}
-                </div>
+                </InvitationCont>
             );
         }
 
         return <LinearLoader loading={this.state.loading} />
+    }
+
+    renderInviteImage() {
+        return this.state.status === 200 
+        ?
+        <img 
+            className="animated fadeIn"
+            src={this.state.invitationData.ownerData.photoUrl} 
+            alt="Room owner avatar" 
+        />
+        : null;
     }
 
     renderInviteActionButton() {
@@ -101,27 +113,48 @@ export default connect(mapStateToProps, mapDispatchToProps)(JoinRoom);
 
 const StyledHeader = styled.div`
     text-align: center;
-    margin-top: 35vh;
 
     h1 {
         font-weight: 800;
-        margin-bottom: 3rem;
-        font-size: 3rem;
+        margin-top: 2rem;
+        font-size: 2.75rem;
     }
 
     h5 {
         font-weight: 100;
-        font-size: 1.5rem;
-        max-width: 500px;
-        margin: 0 auto;
+        font-size: 1.25rem;
+        margin: 1rem;
         color: #9e9e9e;
     }
 `;
 
+const InvitationCont = styled.div`
+    padding: 3rem;
+    box-shadow: 5px 9px 28px rgba(0, 0, 0, 0.2);
+    border-radius: 12px;
+    background-color: #ffffff;
+    max-width: 500px;
+    margin: 25vh auto;
+    position: relative;
+
+    img {
+        position: absolute;
+        top: -40px;
+        left: -25px;
+        border-radius: 50%;
+        min-width: 110px;
+        max-width: 110px;
+        min-height: 110px;
+        max-height: 110px;
+        box-shadow: 5px 9px 28px rgba(0, 0, 0, 0.1);
+        transform: rotate(-7.5deg);
+    }
+`;
+
 const StyledButton = styled.button`
-    transition: 0.3s ease-in-out;
     margin-top: 3rem;
     color: #ffffff;
+    outline: none;
     box-shadow: 0px 9px 28px rgba(0, 0, 0, 0.1);
     border-radius: 4px;
     min-width: 250px;
@@ -132,7 +165,7 @@ const StyledButton = styled.button`
     background: -webkit-linear-gradient(to right, #E100FF, #7F00FF);  /* Chrome 10-25, Safari 5.1-6 */
     background: linear-gradient(to right, #E100FF, #7F00FF); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
-    &:hover, &:active, &:focus {
-        box-shadow: 5px 9px 28px rgba(0, 0, 0, 0.2);
+    &:hover {
+        box-shadow: 5px 9px 28px rgba(0, 0, 0, 0.3);
     }
 `;
