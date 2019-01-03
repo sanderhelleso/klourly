b0bcb25d0c6c760659ab884d9f7cffe77e0a2c55
Sanderhelleso <sanderhelleso@hotmail.com>
2019-01-03 12:53:01 -0800

    refactored avatar and removed css

--- client/src/components/room/OwnerAvatar.js
+++ client/src/components/room/OwnerAvatar.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const OwnerAvatar = props => (
-    <div id="room-owner-avatar">
-         <img 
-            className="animated fadeIn z-depth-3" 
-            src={props.url} 
-        />
-    </div>
-)
+const OwnerAvatar = props => <StyledAvatar className="animated fadeIn z-depth-3" src={props.url} />;
 
 export default OwnerAvatar;
+
+const StyledAvatar = styled.img`
+    position: absolute;
+    bottom: -20%;
+    right: 11%;
+    border-radius: 50%;
+    max-width: 125px;
+    max-height: 125px;
+    min-width: 125px;
+    min-height: 125px;
+`;
