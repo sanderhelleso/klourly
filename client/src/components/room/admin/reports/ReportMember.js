1cec1cbf0ba305f80bd03e6ae46d67d53477a3ea
Sanderhelleso <sanderhelleso@hotmail.com>
2019-01-11 14:20:01 -0800

    now sets room attendie status

--- client/src/components/room/admin/reports/ReportMember.js
+++ client/src/components/room/admin/reports/ReportMember.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 export default class ReportMember extends Component {
     constructor(props) {
         super(props);
+
+        console.log(props);
     }
 
     render() {
--- client/src/components/room/admin/reports/ReportMembers.js
+++ client/src/components/room/admin/reports/ReportMembers.js
@@ -8,10 +8,24 @@ export default class ReportMembers extends Component {
         console.log(props);
     }
 
+    renderMembers() {
+
+        // render member for each room member and set attended status
+        const keys = Object.keys(this.props.data.attendies);
+        return Object.values(this.props.data.roomMembers).map(member => {
+            return  <ReportMember 
+                        data={{
+                            ...member,
+                            attended: keys.indexOf(member.id) !== -1 ? true : false
+                        }}
+                    />
+        });
+    }
+
     render() {
         return (
             <div>
-                
+                {this.renderMembers()}
             </div>
         )
     }
