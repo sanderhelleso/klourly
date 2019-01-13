d94efd84adb9fb00d0d4df97bbca617c991b0436
Sanderhelleso <sanderhelleso@hotmail.com>
2019-01-12 16:03:57 -0800

    refactored JSON downloader to own file and installed jsPDF

--- client/src/components/room/admin/reports/DownloadReports.js
+++ client/src/components/room/admin/reports/DownloadReports.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { format } from '../../../../helpers/format';
 
 // redux
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import DownloadJSON from './downloads/DownloadJSON';
 
 
 class DownloadReports extends Component {
@@ -11,102 +11,12 @@ class DownloadReports extends Component {
         super(props);
     }
 
-    createMembersData() {
-
-        const members = {};
-        Object.values(this.props.activeRoom.membersData).forEach(member => {
-
-            const name = member.name.split('_').join('_');
-            members[name] = 
-            Object.keys(this.props.activeReport.attendies).indexOf(member.id) !== -1
-            ? {
-                name,
-                attended: true,
-                checkedin_at: `${format.tsToDate(this.props.activeReport.attendies[member.id])} ${format.tsToHHMM(this.props.activeReport.attendies[member.id])}`
-            } : { name, attended: false }
-        });
-
-        return Object.values(members).sort((a, b) => b.attended - a.attended);
-
-    }
-
-    reportData() {
-
-        // create meta data for report
-        const now = new Date().getTime();
-        const report_generated_at = `${format.tsToDate(now)} ${format.tsToHHMM(now)}`;
-        const start_time =          `${format.tsToDate(this.props.activeReport.startTime)} ${format.tsToHHMM(this.props.activeReport.startTime)}`;
-        const end_time =            `${format.tsToDate(this.props.activeReport.endTime)} ${format.tsToHHMM(this.props.activeReport.endTime)}`;
-
-
-        return { 
-            meta: { report_generated_at },
-            report: { start_time, end_time, members: this.createMembersData() }
-        }
-    }
-
-    generateJSONReport() {
-
-        const report = {
-            ...this.reportData().meta,
-            report: {
-                total_checkins: Object.keys(this.props.activeReport.attendies).length,
-                attendende_in_percentage: this.props.activeReport.attendenceInPercent,
-                ...this.reportData().report
-            }
-        }
-
-        return JSON.stringify(report, null, 2);
-    }
-
-
-    format(reportType) {
-
-        let fileName;
-        let extension = '.json';
-        let type = 'application/json';
-        let data = this.generateJSONReport();
-
-        // generate filename depending on report type
-        switch(reportType) {
-            case 'checkin':
-                fileName = `checkin-report-${this.props.activeReport.checkinID}`;
-                break;
-        }
-;
-        return { extension, type, fileName, data };
-    }
-
-    downloadJSON(reportType) {
-
-        const format = this.format(reportType);
-
-        // generate a file blob
-        const blob = new Blob([format.data], { ...format.type });
-
-        // if navigator is present, download file immediatly
-        if (window.navigator.msSaveOrOpenBlob) {
-            window.navigator.msSaveBlob(blob, `${format.fileName}${format.extension}`);
-            return;
-        }
-
-        // if navigator is not present, manually create file and download
-        const elem = window.document.createElement('a');
-        elem.href = window.URL.createObjectURL(blob);
-        elem.download = `${format.fileName}${format.extension}`;        
-        document.body.appendChild(elem);
-        elem.click();        
-        document.body.removeChild(elem);
-    }
-
     render() {
         return (
             <div className="download-cont">
                 <h5>Download Report</h5>
                 <div className="col s12 downloads">
-                    <a class="waves-effect waves-purple btn-flat" onClick={() => this.downloadJSON('checkin')}>
-                        JSON
-                    </a>
+                    <DownloadJSON />
                     <a class="waves-effect waves-purple btn-flat">PDF</a>
                 </div>
             </div>
--- client/src/components/room/admin/reports/downloads/DownloadJSON.js
+++ client/src/components/room/admin/reports/downloads/DownloadJSON.js
@@ -1,27 +1,99 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { format } from '../../../../../helpers/format';
 
-export default class DownloadJSON extends Component {
+// redux
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+
+class DownloadJSON extends Component {
     constructor(props) {
         super(props);
     }
 
-    download() {
+    createMembersData() {
 
-        const filename ='test.json'
+        const members = {};
+        Object.values(this.props.activeRoom.membersData).forEach(member => {
+
+            const name = member.name.split('_').join('_');
+            members[name] = 
+            Object.keys(this.props.activeReport.attendies).indexOf(member.id) !== -1
+            ? {
+                name,
+                attended: true,
+                checkedin_at: `${format.tsToDate(this.props.activeReport.attendies[member.id])} ${format.tsToHHMM(this.props.activeReport.attendies[member.id])}`
+            } : { name, attended: false }
+        });
+
+        return Object.values(members).sort((a, b) => b.attended - a.attended);
+
+    }
+
+    reportData() {
+
+        // create meta data for report
+        const now = new Date().getTime();
+        const report_generated_at = `${format.tsToDate(now)} ${format.tsToHHMM(now)}`;
+        const start_time =          `${format.tsToDate(this.props.activeReport.startTime)} ${format.tsToHHMM(this.props.activeReport.startTime)}`;
+        const end_time =            `${format.tsToDate(this.props.activeReport.endTime)} ${format.tsToHHMM(this.props.activeReport.endTime)}`;
+
+
+        return { 
+            meta: { report_generated_at },
+            report: { start_time, end_time, members: this.createMembersData() }
+        }
+    }
+
+    generateJSONReport() {
+
+        const report = {
+            ...this.reportData().meta,
+            report: {
+                total_checkins: Object.keys(this.props.activeReport.attendies).length,
+                attendende_in_percentage: this.props.activeReport.attendenceInPercent,
+                ...this.reportData().report
+            }
+        }
+
+        return JSON.stringify(report, null, 2);
+    }
+
+
+    format(reportType) {
+
+        let fileName;
+        let extension = '.json';
+        let type = 'application/json';
+        let data = this.generateJSONReport();
+
+        // generate filename depending on report type
+        switch(reportType) {
+            case 'checkin':
+                fileName = `checkin-report-${this.props.activeReport.checkinID}`;
+                break;
+        }
+;
+        return { extension, type, fileName, data };
+    }
+
+    downloadJSON(reportType) {
+
+        const format = this.format(reportType);
 
         // generate a file blob
-        const blob = new Blob([JSON.stringify({test: 123}, null, 2)], {type: 'application/json'});
+        const blob = new Blob([format.data], { ...format.type });
 
         // if navigator is present, download file immediatly
         if (window.navigator.msSaveOrOpenBlob) {
-            window.navigator.msSaveBlob(blob, filename);
+            window.navigator.msSaveBlob(blob, `${format.fileName}${format.extension}`);
             return;
         }
 
         // if navigator is not present, manually create file and download
         const elem = window.document.createElement('a');
         elem.href = window.URL.createObjectURL(blob);
-        elem.download = filename;        
+        elem.download = `${format.fileName}${format.extension}`;        
         document.body.appendChild(elem);
         elem.click();        
         document.body.removeChild(elem);
@@ -30,10 +102,23 @@ export default class DownloadJSON extends Component {
     render() {
         return (
             <div>
-                <a class="waves-effect waves-purple btn-flat" onClick={this.download} >
+                <a class="waves-effect waves-purple btn-flat" onClick={() => this.downloadJSON('checkin')}>
                     JSON
                 </a>
             </div>
         )
     }
 }
+
+const mapStateToProps = state => {
+    return { 
+        activeRoom: state.room.activeRoom,
+        activeReport: state.room.activeRoom.activeReport
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DownloadJSON);
