a636d66ee83e4a728615f49022789c5dc5972daa
Sanderhelleso <sanderhelleso@hotmail.com>
2019-01-14 11:13:56 -0800

    now retrieves member data on page load

--- client/src/components/room/admin/reports/member/MemberReport.js
+++ client/src/components/room/admin/reports/member/MemberReport.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
 
-export default class MemberReport extends Component {
+// redux
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+class MemberReport extends Component {
+    constructor(props) {
+        super(props);
+
+        console.log(props);
+    }
+
+    renderMemberReport() {
+
+        if (this.props.userData) {
+            console.log(this.props.userData)
+            return <p>LOADED</p>
+        }
+
+        return <p>Loading...</p>
+    }
+
     render() {
         return (
-            <div>
+            <div className="container">
                 <h1>Member Report</h1>
+                {this.renderMemberReport()}
             </div>
         )
     }
 }
+
+
+const mapStateToProps = (state, props) => {
+
+    // get user
+    return { userData: state.room.activeRoom.membersData
+                        ? Object.values(state.room.activeRoom.membersData)
+                          .filter(member => member.id === props.match.params.memberID)[0]
+                        : null  
+            }
+}
+
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MemberReport);
