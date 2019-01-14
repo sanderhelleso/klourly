ad8f48224780ef0f69a8cdcd2d88af22d82c6ebd
Sanderhelleso <sanderhelleso@hotmail.com>
2019-01-14 12:11:12 -0800

    now renders chartdata and added back to reports from selected member url

--- client/src/components/room/admin/reports/member/MemberReport.js
+++ client/src/components/room/admin/reports/member/MemberReport.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 // redux
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
+import Chart from '../Chart';
+import Back from '../../../../dashboard/Back';
+import DownloadReports from '../downloads/DownloadReports';
+
 class MemberReport extends Component {
     constructor(props) {
         super(props);
-
-        console.log(props);
     }
 
     renderMemberReport() {
 
         if (this.props.userData) {
-            console.log(this.props.userData)
-            return <p>LOADED</p>
+
+            const chartData = {
+                labels: Object.values(this.props.userData.checkins).map(() => this.props.userData.name),
+                dataset: Object.values(this.props.userData.checkins).map(value => value).sort((a, b) => a - b)
+            }
+
+            return(
+                <StyledReport>
+                    <div className="col s12 chart">
+                        <span>Checkins over time</span>
+                        <Chart chartData={chartData} />
+                    </div>
+                    <div className="col s12 details">
+                        <StyledDetails className="col s12">
+                            <div className="col s12 m12 l7">
+                                <DownloadReports />
+                            </div>
+                        </StyledDetails>
+                    </div>
+                </StyledReport>
+            )
         }
 
         return <p>Loading...</p>
@@ -24,7 +46,7 @@ class MemberReport extends Component {
     render() {
         return (
             <div className="container">
-                <h1>Member Report</h1>
+                <Back roomID={this.props.match.params.roomID} location="reports" />
                 {this.renderMemberReport()}
             </div>
         )
@@ -47,3 +69,55 @@ const mapDispatchToProps = dispatch => {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(MemberReport);
+
+
+const StyledReport = styled.div`
+
+    margin: 1rem 2rem;
+    padding: 0 !important;
+    border-radius: 12px;
+    box-shadow: 0px 9px 28px rgba(0, 0, 0, 0.09);
+    background-color: #ffffff;
+
+    .chart {
+
+        position: relative;
+
+        span {
+            position: absolute;
+            display: block;
+            padding: 0.25rem 1rem;
+            top: 15px;
+            right: 15px;
+            color: #ffffff;
+            border-radius: 20px;
+            border: 1px solid #ffffff;
+            font-size: 0.8rem;
+            opacity: 0.5;
+            margin-bottom: 25px;
+        }
+
+        border-top-left-radius: 12px;
+        border-top-right-radius: 12px;
+        padding: 0;
+        padding-top: 70px;
+        background: #B24592;  /* fallback for old browsers */
+        background: -webkit-linear-gradient(to right, #F15F79, #B24592);  /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(to right, #F15F79, #B24592); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    }
+
+    .details {
+        position: relative;
+        padding: 4rem 2rem;
+        min-height: 200px;
+    }
+`;
+
+
+const StyledDetails = styled.div`
+
+    border-bottom: 1px solid #eeeeee;
+    padding-bottom: 2rem !important;
+    margin-bottom: 2rem;
+`;
+
