cbc584e7607736b5ecd50536c5d0cff094dd2c26
Sanderhelleso <sanderhelleso@hotmail.com>
2019-01-18 11:37:55 -0800

    added mapmarker

--- client/src/components/maps/SelectLocationMap.js
+++ client/src/components/maps/SelectLocationMap.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Map from 'pigeon-maps';
+import Marker from 'pigeon-marker'
 import { ReverseGeocoder } from 'open-street-map-reverse-geo-node-client';
 
 // redux
@@ -13,7 +14,12 @@ class SelectLocationMap extends Component {
     constructor(props) {
         super(props);
 
-        console.log(this.props);
+        // initial map config
+        this.MAP_WIDHT = 600;
+        this.MAP_HEIGHT = 400;
+        this.DEFAULT_ZOOM = 14;
+
+        // geocoder and coords
         this.geo = new ReverseGeocoder();
         this.fallbackCoords = [37.386051, -122.083855]; // Mountain View ;)
         this.coords = this.props.userLocation 
@@ -22,18 +28,26 @@ class SelectLocationMap extends Component {
 
         this.state = { 
             locationName: '',
-            loading: false
+            loading: false,
+            markerCoords: this.coords
          }
 
     }
 
+    componentDidMount() {
+        this.reverseGeoCode(this.coords);
+    }
+
     reverseGeoCode = async latLng => {
 
-        //const response = await room.getLocationFromCoords(this.props.userID, latLng);
-        this.setState({ loading: true });
+        // get address from coords
+        this.setState({ 
+            loading: true,
+            markerCoords: latLng
+        });
         const result = await this.geo.getReverse(latLng[0], latLng[1]);
-        console.log(result);
 
+        // update results location
         if (result) this.props.nextStageAction({ locationAddress: result.displayName });
 
     }
@@ -46,11 +60,16 @@ class SelectLocationMap extends Component {
         return (
             <StyledMapCont>
                 <Map 
-                    center={this.coords} 
-                    zoom={12} width={600} 
-                    height={400}
+                    center={this.state.markerCoords} 
+                    zoom={this.DEFAULT_ZOOM} 
+                    width={this.MAP_WIDHT} 
+                    height={this.MAP_HEIGHT}
                     onClick={(e) => this.reverseGeoCode(e.latLng)}
                 >
+                    <Marker 
+                        anchor={this.state.markerCoords}
+                        payload={1} 
+                    />
                 </Map>
             </StyledMapCont>
         )
@@ -79,7 +98,11 @@ const StyledMapCont = styled.div`
     margin-top: 5rem;
 
     div {
-        box-shadow: 0px 18px 56px rgba(0,0,0,0.15);
+        box-shadow: 0px 9px 28px rgba(0,0,0,0.09);
         border-radius: 18px;
+    }
+
+    .pigeon-attribution {
+        display: none;
     }
 `;
