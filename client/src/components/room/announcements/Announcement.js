6cc2c3ce05ac813b4ae8d8e833c81b0941394b23
Sanderhelleso <sanderhelleso@hotmail.com>
2019-01-24 09:13:15 -0800

    added main poll container

--- client/src/components/room/announcements/Announcement.js
+++ client/src/components/room/announcements/Announcement.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { format } from '../../../helpers/format';
+import { Bar } from 'react-chartjs-2';
+import { BarChart2 } from 'react-feather';
 
 // redux
 import { bindActionCreators } from 'redux';
@@ -14,14 +16,38 @@ import BackToRoom from '../BackToRoom';
 class Announcement extends Component {
     constructor(props) {
         super(props);
-    }
 
+        this.options = {
+            maintainAspectRatio: false,
+            scales : {
+                xAxes : [{
+                    gridLines : {
+                        display : false
+                    }
+                }]
+            }
+        };
+
+        this.state = {
+            data: this.props.announcement.poll
+            ? {
+                labels: Object.keys(this.props.announcement.poll.options),
+                datasets: [{
+                    label: '# of Votes',
+                    data: Object.values(this.props.announcement.poll.options),
+                    backgroundColor: ['rgba(179, 136, 255, 0.4)'],
+                    borderColor:['rgba(179, 136, 255, 0.6)'],
+                    borderWidth: 1
+                }]
+            } : false
+        }
+    }
 
     renderAnnouncement() {
 
         if (this.props.announcement) {
             return (
-                <StyledAnnouncement className="animated fadeIn col s12 m10 offset-m1 l8 offset-l2">
+                <StyledAnnouncement className="animated fadeIn col s12 m10 offset-m1 l10 offset-l1">
                     <h1>{this.props.announcement.title}</h1>
                     <h5>{format.getFormatedDateAndTime(this.props.announcement.timestamp)}</h5>
                     <p>{this.props.announcement.message}</p>
@@ -29,11 +55,39 @@ class Announcement extends Component {
                         id={this.props.announcementID} 
                         data={this.props.announcement.reactions} 
                     />
+                    <div className="row">
+                        <div className="col s12">
+                            {this.renderPoll()}
+                        </div>
+                    </div>
                 </StyledAnnouncement>
             )
         }
 
         return <h1>No announcement found</h1>
+    }
+
+    renderPoll() {
+
+        console.log(this.state);
+        if (!this.state.data) return null;
+
+        return (
+            <StyledPoll className="animated fadeIn">
+                <div className="chip">
+                    <BarChart2 size={16} />
+                    Poll
+                </div>
+                <h5>{this.props.announcement.title}</h5>
+                <Bar
+                    className="animated fadeIn"
+                    data={this.state.data}
+                    width={100}
+                    height={270}
+                    options={this.options}
+                />        
+            </StyledPoll>
+        )
     }
 
     render() {
@@ -67,6 +121,12 @@ export default connect(mapStateToProps, mapDispatchToProps)(Announcement);
 
 
 const StyledAnnouncement = styled.section`
+
+    background-color: #ffffff;
+    box-shadow: 0px 9px 28px rgba(0,0,0,0.09);
+    padding: 5rem !important;
+    margin-bottom: 5rem;
+
     h1 {
         font-size: 3rem;
         text-align: center;
@@ -92,5 +152,36 @@ const StyledAnnouncement = styled.section`
 
     .reactions {
         margin-top: 2rem;
+    }
+`;
+
+
+const StyledPoll = styled.div`
+    height: 100%;
+    width: 100%;
+    text-align: left;
+    margin-top: 3rem;
+
+    h5 {
+        font-size: 2rem;
+        color: #363636;
+        text-align: left;
+    }
+
+    .chip {
+        margin-top: 1rem;
+        background-color: #b388ff;
+        color: #ffffff;
+        padding: 0 15px;
+        opacity: 0.8;
+
+        svg {
+            margin: 0 2px -2px 0;
+        }
+    }
+
+    canvas {
+        max-height: 400px;
+        margin-bottom: 2rem;
     }
 `;
