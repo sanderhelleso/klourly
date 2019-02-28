45426baf9094d0fddeddeb126bd090b7a02f309a
Sanderhelleso <sanderhelleso@hotmail.com>
2019-02-28 11:23:30 -0800

    added backend logic for sending forgot password email

--- routes/forgotPassword.js
+++ routes/forgotPassword.js
@@ -12,7 +12,23 @@ module.exports = app => {
     // send a forgot password email
     app.post('/api/auth/sendForgotPasswordEmail', (req, res) => {
 
+        // check if email exists
+        firebase.auth().getUserByEmail(req.body.email)
+        .then(user => {
+
+            // generate id
+            const resetID = shortid.generate();
+
+            // update user ref releated to provided email
+            ref.child(`${user.uid}/forgotPasswordCode`).set({ resetID });
+
+            // send email to user
+            email.sendforgotPassword(req.body.email, resetID, user.uid);
+        })
+        .catch(err => { console.log(err) }); // catch any error, however we dont notify user
+
         // send back success response
+        // send back same response even if no email exists in system due to securtiy
         res.status(200).json({
             success: true,
             message: 'An e-mail containing instruction on how to reset your password has been sent to the provided email!'
